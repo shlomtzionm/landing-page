b4cc472824b9577d97959a1961bb9d64669d83f5
shlomtzionm <shlomtzosh@gmail.com>
2025-04-29 14:48:01 +0300

    more

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,52 +1,57 @@
 import { JSX, useRef, useState } from "react";
 import logo from "../../assets/breather_logo.svg";
 import "./header.css";
-import gsap from "gsap"
+import gsap from "gsap";
+import Menu from "../menu/Menu";
 
 function Header(): JSX.Element {
-const list = useRef<HTMLUListElement| null>(null)
-    function handleMouseIn(){
-        gsap.to(".loginBtn",{
-            background:'white',
-            border:'1px solid green',
-            color:'green'
-        })
-    }
+  const list = useRef<HTMLUListElement | null>(null);
+  const types = useRef<HTMLDivElement | null>(null);
+  function handleMouseIn() {
+    gsap.to(".loginBtn", {
+      background: "white",
+      border: "1px solid green",
+      color: "green",
+    });
+  }
 
-    function handleMouseOut(){
-        gsap.to(".loginBtn",{
-            background:' rgb(16, 108, 73)',
-            border:'1px solid green',
-            color:'white'
-        })
+  function handleMouseOut() {
+    gsap.to(".loginBtn", {
+      background: " rgb(16, 108, 73)",
+      border: "1px solid green",
+      color: "white",
+    });
+  }
 
-    }
+  function handleClickBars() {
+    list.current?.classList.toggle("open");
+  }
 
-    
-    function handleClickBars(){
-      list.current?.classList.toggle("open")
-              }
+  function handleTypesClick() {
+    types.current?.classList.toggle("open");
+  }
   return (
     <div className="header">
-      <img src={logo}></img>
-      
-      <ul ref={list} className="desktop-menu open">
-        <li>Space Types
-        <span className="material-symbols-outlined">
-keyboard_arrow_down
-</span>
+      <img className="logo" src={logo}></img>
+
+      <ul ref={list} className="header-menu">
+        <li className="typesButton" onClick={handleTypesClick}>
+          Space Types
+          <span className="material-symbols-outlined">keyboard_arrow_down</span>
+          <div ref={types} className="menu-component open">
+            <Menu />
+          </div>
         </li>
+
         <li>Find Spaces</li>
         <li>Pricing</li>
         <li>List Your Space</li>
-        <li onMouseEnter={handleMouseIn} 
-        onMouseLeave={handleMouseOut}>
-            <button className="loginBtn" >log In</button></li>
+        <li onMouseEnter={handleMouseIn} onMouseLeave={handleMouseOut}>
+          <button className="loginBtn">log In</button>
+        </li>
       </ul>
-      <div  onClick={handleClickBars} className="bars-container">
-      <span className="material-symbols-outlined hamburger-icon">
-menu
-</span>
+      <div onClick={handleClickBars} className="bars-container">
+        <span className="material-symbols-outlined hamburger-icon">menu</span>
       </div>
     </div>
   );
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,23 +1,35 @@
 import { JSX } from "react";
+import "./menu.css";
+import waitroom from '../../assets/waitroom-n.svg'
+import workspace from '../../assets/workspace-n.svg'
+import building from '../../assets/building-n.svg'
 
 function Menu(): JSX.Element {
   return (
     <div className="menu">
-      
       <ul>
         <li>
+          <img className="menuImg" src={waitroom}></img>
+          <div>
           <h2>conference rooms</h2>
           <p>rent a conference room for part of the entire day.</p>
+          </div>
         </li>
         <li>
+          <img className="menuImg" src={workspace}></img>
+          <div>
           <h2>desks & day passes</h2>
           <p>access to work on desk or table in spaces communal area(s).</p>
+          </div>
         </li>
+
         <li>
+          <img className="menuImg" src={building}></img>
+          <div>
           <h2>private offices</h2>
           <p>rent office space by the city, day, week, month, or for multiple months in a row.</p>
+          </div>
         </li>
-        <li></li>
       </ul>
     </div>
   );
